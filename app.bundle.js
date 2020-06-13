4b10b21c5c5b6042aae610924b8ca961dce17c60
Timothy Gonzalez <timg456789@yahoo.com>
2020-06-13 06:11:16 -0400

    Post receipt.

--- app.bundle.js
+++ app.bundle.js
@@ -22504,11 +22504,14 @@ class PropertyPointOfSaleController {
   initForm() {
     $('#sale-date').prop('disabled', false).val(Moment().format('YYYY-MM-DD'));
     $('#sale-vendor').prop('disabled', false).val('');
-    $('#sale-phone').prop('disabled', false).val('');
     $('#sale-amount-of-account').prop('disabled', false).val('');
     $('#sale-rental-amount').prop('disabled', false).val('');
     $('#sale-payment').prop('disabled', false).val('');
     $('#sale-memo').prop('disabled', false).val('');
+  }
+
+  getCustomer(displayName) {
+    return this.customers.find(x => (x.DisplayName || '').toLocaleLowerCase() === (displayName || '').toLowerCase());
   }
 
   async init(usernameResponse) {
@@ -22522,44 +22525,55 @@ class PropertyPointOfSaleController {
     }
 
     $("#sale-vendor").on('input', function () {
-      let input = this.value;
-      let customerMatch = self.customers.find(x => (x.DisplayName || '').toLocaleLowerCase() === (input || '').toLowerCase());
+      let customer = self.getCustomer(this.value);
 
-      if (customerMatch) {
-        $('#sale-amount-of-account').val(customerMatch.Balance);
-
-        if (customerMatch.PrimaryPhone) {
-          $('#sale-phone').val(customerMatch.PrimaryPhone.FreeFormNumber || '');
-        }
+      if (customer) {
+        $('#sale-amount-of-account').val(customer.Balance);
       }
     });
-    $('#sale-save').click(function () {
-      $('#sale-timestamp').text(Moment().format('L LT'));
-      $('#sale-date-text').text($('#sale-date').val());
-      $('#sale-vendor-text').text($('#sale-vendor').val());
-      let amountOfAccount = (0, _currency.default)($('#sale-amount-of-account').val(), Util.getCurrencyDefaults());
-      let rentalAmount = (0, _currency.default)($('#sale-rental-amount').val(), Util.getCurrencyDefaults());
-      let payment = (0, _currency.default)($('#sale-payment').val(), Util.getCurrencyDefaults());
-      $('#sale-amount-of-account-text').text(Util.format(amountOfAccount.toString()));
-      $('#sale-rental-amount-text').text(Util.format(rentalAmount.toString()));
-      $('#sale-payment-text').text(Util.format(payment.toString()));
-      let balanceDue = amountOfAccount.add(rentalAmount);
-      balanceDue = balanceDue.subtract(payment);
-      $('#sale-balance-due-text').text(Util.format(balanceDue.toString()));
-      let memo = $('#sale-memo').val().trim();
-      $('.memo-receipt-group').toggle(!!memo);
-      $('#sale-memo-text').text(memo);
-      $('#sale-date').prop('disabled', true);
-      $('#sale-vendor').prop('disabled', true);
-      $('#sale-phone').prop('disabled', true);
-      $('#sale-amount-of-account').prop('disabled', true);
-      $('#sale-rental-amount').prop('disabled', true);
-      $('#sale-payment').prop('disabled', true);
-      $('#sale-memo').prop('disabled', true);
-      window.print();
+    $('#sale-save').click(async function () {
+      let receipt = {
+        rentalDate: $('#sale-date').val().trim(),
+        customerId: self.getCustomer($("#sale-vendor").val().trim()).Id,
+        amountOfAccount: $('#sale-amount-of-account').val().trim(),
+        rentalAmount: $('#sale-rental-amount').val().trim(),
+        thisPayment: $('#sale-payment').val().trim(),
+        memo: $('#sale-memo').val().trim()
+      };
+
+      try {
+        let receiptResult = await new DataClient().post('point-of-sale/receipt', receipt);
+        $('#sale-id').text(receiptResult.receipt.id);
+        let timestamp = Moment(receiptResult.receipt.timestamp);
+        $('#sale-timestamp').text(timestamp.format('L LT'));
+        $('#sale-date-text').text(receipt.rentalDate);
+        $('#sale-vendor-text').text($('#sale-vendor').val().trim());
+        let amountOfAccount = (0, _currency.default)(receipt.amountOfAccount, Util.getCurrencyDefaults());
+        $('.amount-of-account-receipt-group').toggle(!!receipt.amountOfAccount);
+        let rentalAmount = (0, _currency.default)(receipt.rentalAmount, Util.getCurrencyDefaults());
+        let payment = (0, _currency.default)(receipt.thisPayment, Util.getCurrencyDefaults());
+        $('#sale-amount-of-account-text').text(Util.format(amountOfAccount.toString()));
+        $('#sale-rental-amount-text').text(Util.format(rentalAmount.toString()));
+        $('#sale-payment-text').text(Util.format(payment.toString()));
+        let balanceDue = amountOfAccount.add(rentalAmount);
+        balanceDue = balanceDue.subtract(payment);
+        $('#sale-balance-due-text').text(Util.format(balanceDue.toString()));
+        $('.memo-receipt-group').toggle(!!receipt.memo);
+        $('#sale-memo-text').text(receipt.memo);
+        $('#sale-date').prop('disabled', true);
+        $('#sale-vendor').prop('disabled', true);
+        $('#sale-amount-of-account').prop('disabled', true);
+        $('#sale-rental-amount').prop('disabled', true);
+        $('#sale-payment').prop('disabled', true);
+        $('#sale-memo').prop('disabled', true);
+        window.print();
+      } catch (error) {
+        alert(error);
+      }
     });
     $('#sale-new').click(function () {
-      self.initForm();
+      // self.initForm(); // should refresh fields only, but I need an accurate balance returned from post receipt.
+      window.location.reload();
     });
   }
 
--- src/controllers/property-point-of-sale-controller.js
+++ src/controllers/property-point-of-sale-controller.js
@@ -13,11 +13,14 @@ export default class PropertyPointOfSaleController {
     initForm() {
         $('#sale-date').prop('disabled', false).val(Moment().format('YYYY-MM-DD'));
         $('#sale-vendor').prop('disabled', false).val('');
-        $('#sale-phone').prop('disabled', false).val('');
         $('#sale-amount-of-account').prop('disabled', false).val('');
         $('#sale-rental-amount').prop('disabled', false).val('');
         $('#sale-payment').prop('disabled', false).val('');
         $('#sale-memo').prop('disabled', false).val('');
+    }
+    getCustomer(displayName) {
+        return this.customers.find(x =>
+            (x.DisplayName || '').toLocaleLowerCase() === (displayName || '').toLowerCase());
     }
     async init(usernameResponse) {
         let self = this;
@@ -28,47 +31,59 @@ export default class PropertyPointOfSaleController {
             $('#sale-vendor-list').append(`<option>${customer.DisplayName}</option>`);
         }
         $("#sale-vendor").on('input', function () {
-            let input = this.value;
-            let customerMatch = self.customers.find(x =>
-                (x.DisplayName || '').toLocaleLowerCase() === (input || '').toLowerCase());
-            if (customerMatch) {
-                $('#sale-amount-of-account').val(customerMatch.Balance);
-                if (customerMatch.PrimaryPhone) {
-                    $('#sale-phone').val(customerMatch.PrimaryPhone.FreeFormNumber || '');
-                }
+            let customer = self.getCustomer(this.value);
+            if (customer) {
+                $('#sale-amount-of-account').val(customer.Balance);
             }
         });
 
-        $('#sale-save').click(function() {
-            $('#sale-timestamp').text(Moment().format('L LT'));
-            $('#sale-date-text').text($('#sale-date').val());
-            $('#sale-vendor-text').text($('#sale-vendor').val());
-            let amountOfAccount = Currency($('#sale-amount-of-account').val(), Util.getCurrencyDefaults());
-            let rentalAmount = Currency($('#sale-rental-amount').val(), Util.getCurrencyDefaults());
-            let payment = Currency($('#sale-payment').val(), Util.getCurrencyDefaults());
-            $('#sale-amount-of-account-text').text(Util.format(amountOfAccount.toString()));
-            $('#sale-rental-amount-text').text(Util.format(rentalAmount.toString()));
-            $('#sale-payment-text').text(Util.format(payment.toString()));
-            let balanceDue = amountOfAccount.add(rentalAmount);
-            balanceDue = balanceDue.subtract(payment);
-            $('#sale-balance-due-text').text(Util.format(balanceDue.toString()));
-            let memo = $('#sale-memo').val().trim();
-            $('.memo-receipt-group').toggle(!!memo);
-            $('#sale-memo-text').text(memo);
+        $('#sale-save').click(async function() {
 
-            $('#sale-date').prop('disabled', true);
-            $('#sale-vendor').prop('disabled', true);
-            $('#sale-phone').prop('disabled', true);
-            $('#sale-amount-of-account').prop('disabled', true);
-            $('#sale-rental-amount').prop('disabled', true);
-            $('#sale-payment').prop('disabled', true);
-            $('#sale-memo').prop('disabled', true);
+            let receipt = {
+                rentalDate: $('#sale-date').val().trim(),
+                customerId: self.getCustomer($("#sale-vendor").val().trim()).Id,
+                amountOfAccount: $('#sale-amount-of-account').val().trim(),
+                rentalAmount: $('#sale-rental-amount').val().trim(),
+                thisPayment: $('#sale-payment').val().trim(),
+                memo: $('#sale-memo').val().trim()
+            };
 
-            window.print();
+            try {
+                let receiptResult = await new DataClient().post('point-of-sale/receipt', receipt);
+                $('#sale-id').text(receiptResult.receipt.id);
+                let timestamp = Moment(receiptResult.receipt.timestamp);
+                $('#sale-timestamp').text(timestamp.format('L LT'));
+                $('#sale-date-text').text(receipt.rentalDate);
+                $('#sale-vendor-text').text($('#sale-vendor').val().trim());
+                let amountOfAccount = Currency(receipt.amountOfAccount, Util.getCurrencyDefaults());
+                $('.amount-of-account-receipt-group').toggle(!!receipt.amountOfAccount);
+                let rentalAmount = Currency(receipt.rentalAmount, Util.getCurrencyDefaults());
+                let payment = Currency(receipt.thisPayment, Util.getCurrencyDefaults());
+                $('#sale-amount-of-account-text').text(Util.format(amountOfAccount.toString()));
+                $('#sale-rental-amount-text').text(Util.format(rentalAmount.toString()));
+                $('#sale-payment-text').text(Util.format(payment.toString()));
+                let balanceDue = amountOfAccount.add(rentalAmount);
+                balanceDue = balanceDue.subtract(payment);
+                $('#sale-balance-due-text').text(Util.format(balanceDue.toString()));
+                $('.memo-receipt-group').toggle(!!receipt.memo);
+                $('#sale-memo-text').text(receipt.memo);
+
+                $('#sale-date').prop('disabled', true);
+                $('#sale-vendor').prop('disabled', true);
+                $('#sale-amount-of-account').prop('disabled', true);
+                $('#sale-rental-amount').prop('disabled', true);
+                $('#sale-payment').prop('disabled', true);
+                $('#sale-memo').prop('disabled', true);
+
+                window.print();
+            } catch (error) {
+                alert(error);
+            }
         });
 
         $('#sale-new').click(function() {
-            self.initForm();
+            // self.initForm(); // should refresh fields only, but I need an accurate balance returned from post receipt.
+            window.location.reload();
         });
     }
 }
